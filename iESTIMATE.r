d1b823f083c98122be4b4c955a4669207ecf7292
Kristian Peters <kristian.peters@korseby.net>
2023-03-13 20:06:25 +0100

    updates to functions

--- iESTIMATE.r
+++ iESTIMATE.r
@@ -48,7 +48,12 @@ tenerife[["class_list"]] <- read.table(file="use-cases/tenerife/class_list.tsv",
 tenerife[["subclass_list"]] <- read.table(file="use-cases/tenerife/subclass_list.tsv", header=TRUE, sep="\t", quote="\"", comment.char="", fill=FALSE, dec=".", stringsAsFactors=FALSE)
 tenerife[["superclass_list"]] <- read.table(file="use-cases/tenerife/superclass_list.tsv", header=TRUE, sep="\t", quote="\"", comment.char="", fill=FALSE, dec=".", stringsAsFactors=FALSE)
 tenerife[["moldes_list"]] <- read.table(file="use-cases/tenerife/moldes_list.tsv", header=TRUE, sep="\t", quote="\"", comment.char="", fill=FALSE, dec=".", stringsAsFactors=FALSE)
-tenerife[["species"]] <- as.factor(as.character(read.table(file="use-cases/tenerife/species_samples.tsv", header=TRUE, sep="\t", quote="\"", comment.char="", fill=FALSE, dec=".", stringsAsFactors=FALSE)))
+tenerife[["div_classes"]] <- read.table(file="use-cases/tenerife/div_classes.tsv", header=TRUE, sep="\t", quote="\"", comment.char="", fill=FALSE, dec=".", stringsAsFactors=FALSE)
+tenerife[["div_subclasses"]] <- read.table(file="use-cases/tenerife/div_subclasses.tsv", header=TRUE, sep="\t", quote="\"", comment.char="", fill=FALSE, dec=".", stringsAsFactors=FALSE)
+tenerife[["div_superclasses"]] <- read.table(file="use-cases/tenerife/div_superclasses.tsv", header=TRUE, sep="\t", quote="\"", comment.char="", fill=FALSE, dec=".", stringsAsFactors=FALSE)
+tenerife[["model_div"]] <- read.table(file="use-cases/tenerife/model_div.tsv", header=TRUE, sep="\t", quote="\"", comment.char="", fill=FALSE, dec=".", stringsAsFactors=FALSE)
+tenerife[["species"]] <- as.factor(as.data.frame(read.table(file="use-cases/tenerife/species_samples.tsv", header=TRUE, sep="\t", quote="\"", comment.char="", fill=FALSE, dec=".", stringsAsFactors=FALSE))[,1])
+tenerife[["colors"]] <- sapply(tenerife$species, function(x) { x <- RColorBrewer::brewer.pal(n=nlevels(tenerife$species), name="Set1")[which(x==levels(tenerife$species))] } )
 usethis::use_data(tenerife, overwrite=TRUE)
 #use_r("tenerife")
 
