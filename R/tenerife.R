e3d406fa91a483744ce621de3630b0c4396e1768
Kristian Peters <kristian.peters@korseby.net>
2023-03-13 17:30:18 +0100

    added reference data

---
+++ R/tenerife.R
@@ -0,0 +1,16 @@
+#' Tenerife reference data
+#'
+#' A dataset containing LC-MS data of plants collected in Tenerife
+#'
+#' @format A list with 7 data frames
+#' \describe{
+#'   \item{tenerife$comp_list}{compound matrix with samples in rows and compounds in columns}
+#'   \item{tenerife$bina_list}{matrix with samples in rows and presence/absence of compounds in columns}
+#'   \item{tenerife$uniq_list}{matrix with samples in rows and compounds that are only present in samples in columns}
+#'   \item{tenerife$class_list}{compound classes matrix with samples in rows and classes in columns}
+#'   \item{tenerife$subclass_list}{compound subclasses matrix with samples in rows and subclasses in columns}
+#'   \item{tenerife$superclass_list}{compound superclass matrix with samples in rows and superclasses in columns}
+#'   \item{tenerife$moldes_list}{molecular descriptors matrix with samples in rows and molecular descriptors in columns}
+#'   ...
+#' }
+"tenerife"
--- iESTIMATE.r
+++ iESTIMATE.r
@@ -23,11 +23,37 @@ setwd("./")
 # Add function from R files
 usethis::use_r("functions.R")
 
-# Load functions
-load_all()
+# Build documentation
+devtools::document()
+
+# README
+#usethis::use_readme_md()
+
+# Unit tests
+#usethis::use_test("shannon.diversity")
+#devtools::test()
 
 # Check requirements
 devtools::check()
+
+# Load functions
+load_all()
+
+# Add reference data
+tenerife <- list()
+tenerife[["comp_list"]] <- read.table(file="use-cases/tenerife/comp_list.tsv", header=TRUE, sep="\t", quote="\"", comment.char="", fill=FALSE, dec=".", stringsAsFactors=FALSE)
+tenerife[["bina_list"]] <- read.table(file="use-cases/tenerife/bina_list.tsv", header=TRUE, sep="\t", quote="\"", comment.char="", fill=FALSE, dec=".", stringsAsFactors=FALSE)
+tenerife[["uniq_list"]] <- read.table(file="use-cases/tenerife/uniq_list.tsv", header=TRUE, sep="\t", quote="\"", comment.char="", fill=FALSE, dec=".", stringsAsFactors=FALSE)
+tenerife[["class_list"]] <- read.table(file="use-cases/tenerife/class_list.tsv", header=TRUE, sep="\t", quote="\"", comment.char="", fill=FALSE, dec=".", stringsAsFactors=FALSE)
+tenerife[["subclass_list"]] <- read.table(file="use-cases/tenerife/subclass_list.tsv", header=TRUE, sep="\t", quote="\"", comment.char="", fill=FALSE, dec=".", stringsAsFactors=FALSE)
+tenerife[["superclass_list"]] <- read.table(file="use-cases/tenerife/superclass_list.tsv", header=TRUE, sep="\t", quote="\"", comment.char="", fill=FALSE, dec=".", stringsAsFactors=FALSE)
+tenerife[["moldes_list"]] <- read.table(file="use-cases/tenerife/moldes_list.tsv", header=TRUE, sep="\t", quote="\"", comment.char="", fill=FALSE, dec=".", stringsAsFactors=FALSE)
+usethis::use_data(tenerife, overwrite=TRUE)
+#use_r("tenerife")
+
+# Vignettes
+#usethis::use_vignette("tenerife")
+devtools::build_vignettes()
 
 # Commit using GIT
 #usethis::use_git()
